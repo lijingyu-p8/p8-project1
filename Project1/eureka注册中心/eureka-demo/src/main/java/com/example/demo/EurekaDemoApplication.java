d56388aa9384148803a7dbe539e23428ded07cc0
lijingyu <lijingyu_dlmu@163.com>
2020-11-13 09:22:26 +0800

    eureka注册中心demo第一次提交

---
+++ Project1/eureka注册中心/eureka-demo/src/main/java/com/example/demo/EurekaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaDemoApplication.class, args);
+	}
+
+}
