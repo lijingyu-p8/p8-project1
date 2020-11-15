e7d143c14cb1fa32ebeb53c4f8f652d0635bbb8b
lijingyu <lijingyu_dlmu@163.com>
2020-11-15 21:10:41 +0800

    异步发送消息

--- Project1/RocketMq/rocketmq-maven/src/main/java/com/study/rocketmq/version01/SyncProducer.java
+++ Project1/RocketMq/rocketmq-maven/src/main/java/com/study/rocketmq/version01/SyncProducer.java
@@ -1,9 +1,12 @@
 package com.study.rocketmq.version01;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.common.message.Message;
 
-public class Producer {
+public class SyncProducer {
 	public static void main(String[] args) throws Exception {
 		// 设置producer组
 		DefaultMQProducer defaultMQProducer = new DefaultMQProducer("producerGroup01");
@@ -15,8 +18,18 @@ public class Producer {
 		// topic 消息将要发送到的地址
 		// body 消息中的具体数据
 		Message msg = new Message("topic01", message.getBytes());
+		// 同步消息发送，等到broker的ack之后才会结束
 		defaultMQProducer.send(msg);
-		//关闭连接
+
+		// 可以批量发送
+		// 批量消息要求必要具有同一topic、相同消息配置
+		// 不支持延时消息
+		// 建议一个批量消息最好不要超过1MB大小
+		// 如果不确定是否超过限制，可以手动计算大小分批发送
+		List<Message> list = new ArrayList<>();
+		list.add(msg);
+		defaultMQProducer.send(list);
+		// 关闭连接
 		defaultMQProducer.shutdown();
 	}
 }
