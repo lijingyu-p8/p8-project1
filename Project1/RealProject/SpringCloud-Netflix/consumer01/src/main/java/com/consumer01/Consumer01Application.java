0949ad18dff2f68ca0ab2b166326a15f9991fda1
lijingyu <lijingyu_dlmu@163.com>
2021-05-30 11:43:03 +0800

    增加consumer

---
+++ Project1/RealProject/SpringCloud-Netflix/consumer01/src/main/java/com/consumer01/Consumer01Application.java
@@ -0,0 +1,13 @@
+package com.consumer01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Consumer01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Consumer01Application.class, args);
+	}
+
+}
