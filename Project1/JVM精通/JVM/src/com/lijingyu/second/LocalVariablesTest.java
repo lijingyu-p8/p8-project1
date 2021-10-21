3f41a76eca2ff6e0916e9ad11e8404a14e6f12d7
lijingyu <lijingyu_dlmu@163.com>
2021-10-21 22:19:37 +0800

    jvm笔记

---
+++ Project1/JVM精通/JVM/src/com/lijingyu/second/LocalVariablesTest.java
@@ -0,0 +1,34 @@
+package com.lijingyu.second;
+
+public class LocalVariablesTest {
+    public int test() {
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        return c;
+    }
+
+    public int test2(String str) {
+        int a = 1;
+        long d = 111l;
+        int b = 2;
+        int c = a + b;
+        return c;
+    }
+
+    public static int test3() {
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        return c;
+    }
+}
+
+
+
+
+
+
+
+
+
