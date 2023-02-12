648accc05342cd33b665ee9dd84d8857e9085ad2
lijingyu-p8 <lijingyu_dlmu@163.com>
2023-02-12 12:23:51 +0800

    init project
    init project

---
+++ minyu-knowledge-sea/src/main/java/com/minyu/knowledge/sea/MinyuKnowledgeSeaApplication.java
@@ -0,0 +1,13 @@
+package com.minyu.knowledge.sea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinyuKnowledgeSeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinyuKnowledgeSeaApplication.class, args);
+	}
+
+}
---
+++ minyu-knowledge-sea/src/test/java/com/minyu/knowledge/sea/MinyuKnowledgeSeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minyu.knowledge.sea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinyuKnowledgeSeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
