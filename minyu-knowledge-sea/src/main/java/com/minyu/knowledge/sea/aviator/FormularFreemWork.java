ec798df525681eb7ce0a78fb81d5d23e1485d521
lijingyu-p8 <lijingyu_dlmu@163.com>
2023-02-19 18:33:44 +0800

    添加Kafka pardition
    pardition

---
+++ minyu-knowledge-sea/src/main/java/com/minyu/knowledge/sea/aviator/FormularFreemWork.java
@@ -0,0 +1,9 @@
+package com.minyu.knowledge.sea.aviator;
+
+/**
+ * @Description: 公式框架
+ * @Author: lijingyu
+ * @CreateTime: 2023-02-19  18:31
+ */
+public class FormularFreemWork {
+}
---
+++ minyu-knowledge-sea/src/main/java/com/minyu/knowledge/sea/kafka/SelfPartitioner.java
@@ -0,0 +1,33 @@
+package com.minyu.knowledge.sea.kafka;
+
+import org.apache.kafka.clients.producer.Partitioner;
+import org.apache.kafka.common.Cluster;
+import org.apache.kafka.common.PartitionInfo;
+import org.apache.kafka.common.utils.Utils;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Description: 自定义分区器，以value值进行分区
+ * @Author: lijingyu
+ * @CreateTime: 2023-02-19  18:32
+ */
+public class SelfPartitioner implements Partitioner {
+    public int partition(String topic, Object key, byte[] keyBytes,
+                         Object value, byte[] valueBytes, Cluster cluster) {
+        List<PartitionInfo> partitionInfos = cluster.partitionsForTopic(topic);
+        int num = partitionInfos.size();
+        int parId = Utils.toPositive(Utils.murmur2(valueBytes)) % num;//来自DefaultPartitioner的处理
+        return parId;
+    }
+
+    public void close() {
+        //do nothing
+    }
+
+    public void configure(Map<String, ?> configs) {
+        //do nothing
+    }
+
+}
